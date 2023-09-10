c38bfa890e02054d1a072ccbda81678f309f407e
James Abdy <jambdy@gmail.com>
2023-09-10 18:04:19 -0400

    update api for protected web content

--- static/js/custom-scripts.js
+++ static/js/custom-scripts.js
@@ -2,9 +2,7 @@ function getContent(id){
 	/* Query stored content in DynamodDB table
 	   Implemented to prevent content appearing in search engines */
 	let pw = prompt("Please enter password");
-	if(pw === 'MSProjects') {
-		$.get( "https://s3boc3luof.execute-api.us-east-1.amazonaws.com/prod/items/"+id, function( data ) {
-    		$( "#content_main" ).html(data.Content);
-    	});
-	}
+    $.get( "https://zxzx0j2dth.execute-api.us-east-1.amazonaws.com/prod/items/"+id+"?password="+pw, function( data ) {
+        $( "#content_main" ).html(data.Content);
+    });
 }  
