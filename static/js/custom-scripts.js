bc050953ce49e4e5ef6afff64037b1c2332500c5
James Abdy <jambdy@gmail.com>
2023-09-10 12:27:22 -0400

    add password to view master project

--- static/js/custom-scripts.js
+++ static/js/custom-scripts.js
@@ -1,9 +1,10 @@
 function getContent(id){  
 	/* Query stored content in DynamodDB table
-	   Implemented to prevent content appearing in search engines */	
-	console.log("Hello James1");  
-	//$( "#content_main" ).text("hefjlkdj");
-	$.get( "https://s3boc3luof.execute-api.us-east-1.amazonaws.com/prod/items/"+id, function( data ) {
-		$( "#content_main" ).html(data.Content);
-	});
+	   Implemented to prevent content appearing in search engines */
+	let pw = prompt("Please enter password");
+	if(pw === 'MSProjects') {
+		$.get( "https://s3boc3luof.execute-api.us-east-1.amazonaws.com/prod/items/"+id, function( data ) {
+    		$( "#content_main" ).html(data.Content);
+    	});
+	}
 }  
