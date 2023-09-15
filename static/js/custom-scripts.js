7512c50c66eddadb822295fa737f853d0de592a9
James Abdy <jambdy@gmail.com>
2023-09-14 23:41:02 -0400

    Update content api response handling

--- static/js/custom-scripts.js
+++ static/js/custom-scripts.js
@@ -3,6 +3,6 @@ function getContent(id){
 	   Implemented to prevent content appearing in search engines */
 	let pw = prompt("Please enter password");
     $.get( "https://zxzx0j2dth.execute-api.us-east-1.amazonaws.com/prod/items/"+id+"?password="+pw, function( data ) {
-        $( "#content_main" ).html(data.Content);
+        $( "#content_main" ).html(data.Content.S);
     });
 }  
